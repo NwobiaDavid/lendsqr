7be489d35f72765adcddeefa65653766e8e4f9bd
NwobiaDavid <dnwobia@gmail.com>
2023-06-03 09:16:09 +0100

    made the page title and header component dynamic

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,51 @@
-import './App.css'
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes ,Link} from 'react-router-dom';
+import React, {useEffect} from 'react';
+import { BrowserRouter as Router, Route, Routes, Link, useLocation } from 'react-router-dom';
 import LoginPage from './components/LoginPage/LoginPage';
 import DashboardPage from './components/DashboardPage/DashboardPage';
 import UserPage from './components/UserPage/UserPage';
 import UserDetailsPage from './components/UserDetailsPage/UserDetailsPage';
-
+import Header from './components/partials/Header';
 
 const App: React.FC = () => {
-  
+  const location = useLocation();
+  const isLoginPage = location.pathname === '/';
+
+  useEffect(() => {
+    // Update the document title whenever the location changes
+    document.title = `My App | ${location.pathname.substring(1).toUpperCase()}`;
+  }, [location]);
+
   return (
-    <Router>
+   
       <div>
+        {/* Conditionally render the Header component */}
+        {!isLoginPage && <Header />}
+
         {/* Navigation */}
         <ul>
           <li>
             <Link to="/">Home</Link>
           </li>
           <li>
-            <Link to="/dashboard">dashboard</Link>
+            <Link to="/dashboard">Dashboard</Link>
           </li>
           <li>
-            <Link to="/users">users</Link>
+            <Link to="/users">Users</Link>
           </li>
           <li>
-            <Link to="/user/:id">users details</Link>
+            <Link to="/user/:id">User Details</Link>
           </li>
         </ul>
 
         {/* Routes */}
-        <Routes >
-        <Route path="/" element={<LoginPage/>} />
-        <Route path="/dashboard" element={<DashboardPage/>} />
-        <Route path="/users" element={<UserPage/>} />
-        <Route path="/user/:id" element={<UserDetailsPage/>} />
-      </Routes>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/dashboard" element={<DashboardPage />} />
+          <Route path="/users" element={<UserPage />} />
+          <Route path="/user/:id" element={<UserDetailsPage />} />
+        </Routes>
       </div>
-    </Router>
+    
   );
 };
 
---
+++ src/components/partials/Header.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './parials.css';
+
+export default function Header() {
+  return (
+    <div className='header'>
+      this is the header + +
+    </div>
+  )
+}
